42a776fcc863d4ab6463872ebd725ff6167adcfc
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-04-14 13:39:47 -0300

    microsserviço de autenticação

--- auth/src/test/java/com/klayrocha/auth/AuthApplicationTests.java
+++ auth/src/test/java/com/klayrocha/auth/AuthApplicationTests.java
@@ -1,4 +1,4 @@
-package com.spiet.auth;
+package com.klayrocha.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AuthApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
